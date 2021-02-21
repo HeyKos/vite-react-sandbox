bf24c529452a47666e663e5d1703bb98e0a27566
mikekoser <mikekoser@gmail.com>
2021-02-21 14:10:26 -0500

    Swapped login form to use Formik and Bootstrap.

--- src/pages/auth/login-page.tsx
+++ src/pages/auth/login-page.tsx
@@ -1,35 +1,36 @@
-import React, { useState, useContext } from "react";
-import { Redirect, useHistory } from "react-router-dom";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import firebase from "firebase-init";
 import "firebase/auth";
 import "firebase/firestore";
 import { AuthContext } from "AuthProvider";
 import { siteMap } from "sitemap";
+import { Formik, Field, Form, FormikHelpers } from 'formik';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormLabel from "react-bootstrap/FormLabel";
 
-interface UserData {
+interface LoginData {
     email: string;
     password: string;
 }
 const Login = () => {
     const authContext = useContext(AuthContext);
     const history = useHistory();
-    const [values, setValues] = useState({
-        email: "",
-        password: ""
-    } as UserData);
-
     const handleClick = () => history.push(siteMap.auth.signup);
 
-    const handleChange = (event: any) => {
-        event.persist();
-        setValues(values => ({
-            ...values,
-            [event.target.name]: event.target.value
-        }));
-   };
+//     const handleChange = (event: any) => {
+//         event.persist();
+//         setValues(values => ({
+//             ...values,
+//             [event.target.name]: event.target.value
+//         }));
+//    };
 
-   const handleSubmit = (event: any) => {
-      event.preventDefault();
+   const handleSubmit = (values: LoginData) => {
       firebase
         .auth()
         .signInWithEmailAndPassword(values.email, values.password)
@@ -44,21 +45,64 @@ const Login = () => {
         });
    }
 
-//    if (authContext.authenticated) {
-//        return <Redirect to={siteMap.root} />;
-//    }
-
   return (
-    <div style={{textAlign: 'center'}}>
-        <h1>Login</h1>
-        <form onSubmit={handleSubmit}>
-            <input type="text" name="email" value={values.email} placeholder="Enter your Email" onChange={handleChange} /><br /><br />
-            <input type="password" name="password" value={values.password} placeholder="Enter your Password" onChange={handleChange} /><br /><br />
-            <button>Login</button>
-            <p>Not logged in yet?</p>
-            <button onClick={handleClick}>SignUp</button>
-        </form>
-    </div>
+    <Container>
+        <Row>
+            <Col>
+                <h1>Login</h1>
+            </Col>
+        </Row>
+        <Row>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                }}
+                onSubmit={(
+                    values: LoginData,
+                    { setSubmitting }: FormikHelpers<LoginData>
+                ) => {
+                    handleSubmit(values);
+                }}
+                >
+                <Form>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <FormGroup controlId="emailField">
+                                    <FormLabel>Email Address</FormLabel>
+                                    <Field class="form-control" id="email" name="email" type="email" placeholder="Enter your Email" />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <FormGroup controlId="passwordField">
+                                    <FormLabel>Password</FormLabel>
+                                    <Field class="form-control" id="password" name="password" type="password" placeholder="Enter your Password" />
+                                </FormGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Button type="submit">Login</Button>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p>Not logged in yet?</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Button onClick={handleClick}>SignUp</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Form>
+            </Formik>
+        </Row>
+    </Container>
   );
 };
 
