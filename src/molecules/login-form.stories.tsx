4d3659fdbb6f71108e2307453f7bd275da6cd961
mikekoser <mikekoser@gmail.com>
2021-02-27 13:22:02 -0500

    Created story for login form. Updated the storybook config to allow absolute imports with typescript.

---
+++ src/molecules/login-form.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import LoginForm, { LoginFormProperties } from './login-form';
+
+export default {
+  title: "Molecules/Login Form",
+  component: LoginForm,
+} as Meta;
+
+export const LoginFormTemplate: Story<LoginFormProperties> = (args) => <LoginForm {...args} />
--- src/molecules/login-form.tsx
+++ src/molecules/login-form.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Field, Form, Formik, FormikHelpers } from "formik";
+import { Form, Formik, FormikHelpers } from "formik";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
